2a800ce12687c27fa4f21ce465fc7f5aff8a7d3c
gguyon0925 <gguyon0925@gmail.com>
2025-02-01 09:58:40 -0500

    fuzzy search

--- app/services/firebase_service.py
+++ app/services/firebase_service.py
@@ -39,3 +39,50 @@ class FirebaseService:
             raise HTTPException(status_code=404, detail="User not found")
         doc_ref.delete()
         return {"message": "User deleted successfully"}
+
+    async def get_searchable_players(self):
+        try:
+            players_ref = self.db.collection('players')
+            docs = players_ref.stream()
+
+            return [{
+                "id": doc.get("id"),
+                "name": doc.get("mlb_person_fullName"),
+                "metadata": {
+                    "jersey_number": doc.get("mlb_jerseyNumber"),
+                    "position": {
+                        "name": doc.get("mlb_position_name"),
+                        "abbreviation": doc.get("mlb_position_abbreviation"),
+                        "type": doc.get("mlb_position_type")
+                    },
+                    "status": doc.get("mlb_status_description"),
+                    "mlb_id": doc.get("mlb_person_id"),
+                    "team_id": doc.get("team_id")
+                }
+            } for doc in docs]
+        except Exception as e:
+            raise Exception(f"Failed to get players: {str(e)}")
+
+    async def get_searchable_teams(self):
+        try:
+            teams_ref = self.db.collection('teams')
+            docs = teams_ref.stream()
+
+            return [{
+                "id": doc.get("id"),
+                "name": doc.get("mlb_name"),
+                "metadata": {
+                    "location": doc.get("mlb_locationName"),
+                    "short_name": doc.get("mlb_shortName"),
+                    "team_name": doc.get("mlb_teamName"),
+                    "abbreviation": doc.get("mlb_abbreviation"),
+                    "mlb_id": doc.get("mlb_id"),
+                    "season": doc.get("mlb_season"),
+                    "spring_league": {
+                        "name": doc.get("mlb_springLeague_name"),
+                        "abbreviation": doc.get("mlb_springLeague_abbreviation")
+                    }
+                }
+            } for doc in docs]
+        except Exception as e:
+            raise Exception(f"Failed to get teams: {str(e)}")
